31a14634b3cf56823bab1be422432e61adb4f6fc
Felix Wiemann <FelixWiemann@users.noreply.github.com>
2024-01-21 16:02:07 +0100

    add help text for aurman

--- aurman.sh
+++ aurman.sh
@@ -24,10 +24,28 @@ install () {
 	makepkg -sic
 }
 
-while getopts "ui:" opt; do
+help () {
+	echo "
+Script for managing all the AURs. 
+Requires a folder ~/AUR/ to exist.
+All AURs will be installed with as a git repo clone into this folder. updating will update all AURs in that folder
+usage: aurman [option] 
+      	-u: 		update existing packages
+   	-i <arg>: 	install AUR package from <args> git repo link
+       	-h: 		show this helptext"
+}
+
+# unsetting name in case it was set
+noargs="true"
+while getopts "hui:" opt; do
 	case "$opt" in
 		"i" ) install $OPTARG;;
 		"u" ) update;;
+		"h" | * ) help;;
 	esac
+	noargs="false"
 done
+
+[[ "$noargs" == "true" ]] && help
+
 exit 0
